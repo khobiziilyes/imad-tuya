3299b823165c06732ebf9dfe0cd2697a98cbe41d
Ilyes Khobizi <khobizi.ilyes@gmail.com>
2025-04-16 02:10:27 +0100

    switchBrightness

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 import { TuyaContext } from "@tuya/tuya-connector-nodejs";
 
+async function getProperty(device_id, code) {
+	return await tuya
+		.request({
+			path: `/v2.0/cloud/thing/${device_id}/shadow/properties?codes=${code}`,
+			method: "GET",
+		})
+		.then(res => res.result.properties[0].value);
+}
+
+async function setBrightness(device_id, value) {
+	const commands = await tuya.request({
+		path: `/v2.0/cloud/thing/${device_id}/shadow/properties/issue`,
+		method: "POST",
+		body: {
+			properties: JSON.stringify({ bright_value: value }),
+		},
+	});
+
+	console.log("Executed command:", commands);
+}
+
+async function getBrightness(device_id) {
+	return await getProperty(device_id, "bright_value");
+}
+
 async function setLed(device_id, value) {
 	const commands = await tuya.request({
 		path: `/v2.0/cloud/thing/${device_id}/shadow/properties/issue`,
@@ -13,17 +38,17 @@ async function setLed(device_id, value) {
 }
 
 async function getLed(device_id) {
-	return await tuya
-		.request({
-			path: `/v2.0/cloud/thing/${device_id}/shadow/properties?codes=switch_led`,
-			method: "GET",
-		})
-		.then(res => res.result.properties[0].value);
+	return await getProperty(device_id, "switch_led");
 }
 
 async function switchLed(device_id) {
 	const currVal = await getLed(device_id);
 	await setLed(device_id, !currVal);
+}
+
+async function switchBrightness(device_id) {
+	const currVal = await getBrightness(device_id);
+	await setBrightness(device_id, currVal > 900 ? 10 : currVal + 200);
 }
 
 const tuya = new TuyaContext({
@@ -43,4 +68,4 @@ const {
 
 console.log("Device ID:", device_id);
 
-await switchLed(device_id);
+await switchBrightness(device_id);
